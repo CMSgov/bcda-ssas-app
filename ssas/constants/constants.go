f5823bf34e89fd4c8ab1d93629ebacfc0aa9d0a3
Matt Snook <matt.snook@adhocteam.us>
2019-11-04 22:28:46 -0500

    BCDA-2243: Update commentary.

--- ssas/constants/constants.go
+++ ssas/constants/constants.go
@@ -1,4 +1,4 @@
 package constants
 
-// This is set during compilation.  See build_and_package.sh in the ops repo
+// This is set during compilation.  See build_and_package.sh in the /ops directory
 var Version = "latest"
