09ec266f8887b351218205c9c3490eb66deb6d43
Will Huang <52384927+whuang85@users.noreply.github.com>
2021-06-01 20:03:18 -0500

    patch group for v2

--- ssas/service/admin/router.go
+++ ssas/service/admin/router.go
@@ -50,8 +50,9 @@ func routes() *chi.Mux {
 
 	r.Route("/v2", func(r chi.Router) {
 		r.With(requireBasicAuth).Post("/group", createGroup)
-    r.With(requireBasicAuth).Post("/system/{systemID}/ip", registerIP)
-	  r.With(requireBasicAuth).Get("/system/{systemID}/ip", getSystemIPs)
+		r.With(requireBasicAuth).Patch("/group", updateGroup)
+		r.With(requireBasicAuth).Post("/system/{systemID}/ip", registerIP)
+		r.With(requireBasicAuth).Get("/system/{systemID}/ip", getSystemIPs)
 	})
 
 	swaggerPath := "./swaggerui"
