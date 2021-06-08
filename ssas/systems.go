efc1a16c9f01f82fae7a8f5db95f78498fa464fb
Gavin St. Ours <gst-ours@fearless.tech>
2021-06-08 13:39:15 -0400

    Soft-delete specified IP by system ID

--- ssas/systems.go
+++ ssas/systems.go
@@ -7,6 +7,9 @@ import (
 	"encoding/pem"
 	"errors"
 	"fmt"
+	"github.com/CMSgov/bcda-ssas-app/ssas/cfg"
+	"github.com/pborman/uuid"
+	"gorm.io/gorm"
 	"io"
 	"io/ioutil"
 	"net"
@@ -14,9 +17,6 @@ import (
 	"strconv"
 	"strings"
 	"time"
-	"github.com/CMSgov/bcda-ssas-app/ssas/cfg"
-	"github.com/pborman/uuid"
-	"gorm.io/gorm"
 )
 
 var DefaultScope string
@@ -546,6 +546,38 @@ func (system *System) GetIps(trackingID string) ([]IP, error) {
 	var ips []IP
 	db.Find(&ips, "system_id=? AND deleted_at IS NULL", system.ID)
 	return ips, nil
+}
+
+// DeleteIP soft-deletes an IP associated with a specific system
+func (system *System) DeleteIP(ipID string, trackingID string) (IP, error) {
+	var (
+		db  = GetGORMDbConnection()
+		ip  IP
+		err error
+	)
+	defer Close(db)
+
+	regEvent := Event{Op: "DeleteIP", TrackingID: trackingID, Help: "calling from ssas.DeleteIP()"}
+	OperationStarted(regEvent)
+
+	// Find IP to delete
+	err = db.First(&ip, "system_id = ? AND id = ?", system.ID, ipID).Error
+	if err != nil {
+		regEvent.Help = fmt.Sprintf("Unable to find IP address with ID %s: %s", ipID, err)
+		OperationFailed(regEvent)
+		return IP{}, fmt.Errorf("Unable to find IP address with ID %s: %s", ipID, err)
+	}
+
+	// Soft delete IP
+	// Note: db.Delete() soft-deletes by default because the DeletedAt field is set on the Gorm model that IP inherits
+	err = db.Delete(&ip).Error
+	if err != nil {
+		regEvent.Help = fmt.Sprintf("Unable to delete IP address with ID %s: %s", ipID, err)
+		OperationFailed(regEvent)
+		return IP{}, fmt.Errorf("Unable to delete IP address with ID %s: %s", ipID, err)
+	}
+
+	return ip, nil
 }
 
 // DataForSystem returns the group extra data associated with this system
