d44ee668ee0cd5459c64a05adc342047414d767b
Will Huang <52384927+whuang85@users.noreply.github.com>
2021-06-21 14:07:04 -0500

    remove foreign key

--- db/migrations/000007_create_root_key_update_client_token.down.sql
+++ db/migrations/000007_create_root_key_update_client_token.down.sql
@@ -1,5 +1,4 @@
 BEGIN;
-ALTER TABLE client_tokens DROP CONSTRAINT IF EXISTS client_tokens_uuid_root_keys_uuid_foreign;
 ALTER TABLE client_tokens DROP COLUMN IF EXISTS expires_at;
 DROP TABLE root_keys;
 COMMIT;
--- db/migrations/000007_create_root_key_update_client_token.up.sql
+++ db/migrations/000007_create_root_key_update_client_token.up.sql
@@ -22,8 +22,5 @@ ALTER SEQUENCE public.root_keys_id_seq OWNED BY public.root_keys.id;
 ALTER TABLE public.client_tokens
 ADD COLUMN expires_at timestamp with time zone;
 
-ALTER TABLE public.client_tokens
-ADD CONSTRAINT client_tokens_uuid_root_keys_uuid_foreign FOREIGN KEY (uuid) REFERENCES public.root_keys(uuid) ON UPDATE RESTRICT ON DELETE RESTRICT;
-
 END;
 COMMIT;
