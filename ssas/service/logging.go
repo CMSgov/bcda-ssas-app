d09d3c85ae5e43b32d9bd7f77ba275e2e23f7d59
karinamzalez <karinamzalez@gmail.com>
2024-02-14 13:41:48 -0600

    add integration test for gettransactionId

--- ssas/service/logging.go
+++ ssas/service/logging.go
@@ -90,11 +90,3 @@ type CtxTransactionKeyType string
 
 // context.Context key to get the transaction ID from the request context
 const CtxTransactionKey CtxTransactionKeyType = "ctxTransaction"
-
-// Adds a transaction ID to the request context
-func GetTransactionID(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		r = r.WithContext(context.WithValue(r.Context(), CtxTransactionKey, r.Header.Get("transaction_id")))
-		next.ServeHTTP(w, r)
-	})
-}
