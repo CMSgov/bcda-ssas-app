7c50dfbf80606918f24c89d39ae1baf593c928d4
Lauren Krugen <laurenkrugen@navapbc.com>
2025-12-22 13:33:50 -0700

    add ./lambda to sonarqube coverage

--- unit_test.sh
+++ unit_test.sh
@@ -31,7 +31,16 @@ DATABASE_URL=$TEST_DB_URL DEBUG=true go run github.com/CMSgov/bcda-ssas-app/ssas
 echo "Successfully loaded SSAS fixture data"
 
 echo "Running SSAS unit tests and placing results/coverage in test_results/${timestamp} on host..."
-DATABASE_URL=$TEST_DB_URL QUEUE_DATABASE_URL=$TEST_DB_URL gotestsum --debug --junitfile test_results/${timestamp}/junit.xml -- -cover -coverpkg ./... -p 1 -race ./ssas/... -coverprofile test_results/${timestamp}/testcoverage-ssas.out 2>&1 | tee test_results/${timestamp}/testresults.out
+DATABASE_URL=$TEST_DB_URL QUEUE_DATABASE_URL=$TEST_DB_URL 
+PACKAGES_TO_COVER=$(go list ./... ./lambda | egrep -v 'test|mock|db/migrations' | tr "\n" "," | sed -e 's/,$//g')
+gotestsum \
+    --debug \
+    --junitfile test_results/${timestamp}/junit.xml -- \
+    -cover \
+    -coverpkg ${PACKAGES_TO_COVER} \
+    -p 1 \
+    -race ./ssas/... \
+    -coverprofile test_results/${timestamp}/testcoverage-ssas.out 2>&1 | tee test_results/${timestamp}/testresults.out
 go tool cover -func test_results/${timestamp}/testcoverage-ssas.out > test_results/${timestamp}/testcov_byfunc.out
 echo TOTAL COVERAGE:  $(tail -1 test_results/${timestamp}/testcov_byfunc.out | head -1)
 go tool cover -html=test_results/${timestamp}/testcoverage-ssas.out -o test_results/${timestamp}/testcoverage.html
